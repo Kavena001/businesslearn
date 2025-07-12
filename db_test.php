9f405b867b2979a7e008f872620ee088ea6ffac9
Kavena001 <alainkavena@gmail.com>
2025-07-12 17:41:21 -0400

    testing database connection

--- db_test.php
+++ db_test.php
@@ -1,6 +1,6 @@
 <?php
 // Use credentials from Hostinger's MySQL Databases section
-$db = new mysqli('localhost', 'u1234_yourdbuser', 'YourActualPassword', 'u1234_yourdbname');
+$db = new mysqli('localhost', 'u189409396_Kavena2025', 'Adminphoenix25', 'u189409396_formation_prof');
 
 if ($db->connect_error) {
     die("DB FAIL: " . $db->connect_error);
